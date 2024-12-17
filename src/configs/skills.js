0325194c35f02da8ab0d05fa9fe964d3f8b50066
tonynguyen <tonyjsfreelancer@gmail.com>
2024-12-18 00:06:23 +0700

    first commit

---
+++ src/configs/skills.js
@@ -0,0 +1,98 @@
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  refine: "refine",
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi,
+  },
+
+  {
+    title: "Redux Js",
+    icon: IconKeys.redux,
+  },
+  {
+    title: "Styled Components",
+    icon: IconKeys.styled,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "Radix UI",
+    icon: IconKeys.radix,
+  },
+  {
+    title: "Shadcn UI",
+    icon: IconKeys.shadcn,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+  {
+    title: "Refine",
+    icon: IconKeys.refine,
+  },
+];
